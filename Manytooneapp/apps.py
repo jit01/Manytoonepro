465bce0c169ca4e8cd6f9ae14e8c20a1048b2ad3
jit01 <adhulum@gmail.com>
2019-04-07 14:31:52 +0530

    Initial commit

---
+++ Manytooneapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ManytooneappConfig(AppConfig):
+    name = 'Manytooneapp'
---
+++ Manytooneapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class student(models.Model):
+    sname = models.CharField(max_length=20)
+    sloc = models.CharField(max_length=20)
+    sage = models.IntegerField()
+    def __str__(self):
+        return self.sname
+
+class course(models.Model):
+    cid_id = models.ForeignKey(student)
+    cname = models.CharField(max_length=20)
+    fee = models.IntegerField()
+    def __str__(self):
+        return self.cname
